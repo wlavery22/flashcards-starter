5f7de8cbf1a4f58ebd8316644c31ca4af9de2b7d
William Lavery <lavery_william@yahoo.com>
2023-10-12 23:09:48 -0400

    Add endRound function

--- src/card.js
+++ src/card.js
@@ -50,7 +50,9 @@ const evaluateGuess = (guess, round) => {
 };
 
 const endRound = (round) => {
-  console.log(`** Round over! ** You answered`)
+  const finalMessage = (`** Round over! ** You answered ${calculatePercentCorrect(round)}% of the questions correctly!`)
+  console.log(finalMessage)
+  return finalMessage 
 }
 
 function calculatePercentCorrect(round) {
@@ -61,8 +63,6 @@ function calculatePercentCorrect(round) {
   return percentCorrect
 }
 
-// endRound(round): prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
-
 module.exports = {
   createCard,
   evaluateGuess,
@@ -72,83 +72,3 @@ module.exports = {
   createRound,
   calculatePercentCorrect
 }
-
-// const createRound = (deck) => {
-//   let round = {
-//     deck: deck,
-//     currentCard: deck[0],
-//     turns: 0,
-//     incorrectGuesses: [],
-//     // currentCardIndex: currentCardIndex
-//   }
-//   console.log(round.currentCard)
-//   // currentCardIndex = 0, turns = 0, incorrectGuesses = []
-//   round.currentCard = deck.currentCardIndex
-//   console.log(round.currentCard)
-//   return round;
-// }
-
-// const totalIncorrectGuesses = round.incorrectGuesses.length;
-// const correctGuesses = totalGuesses - totalIncorrectGuesses;
-// console.log('Your project is running...'); 
-// const calculatePercentCorrect = round => {
-//   const numOfQuestions = countCards(round.deck);
-//   const numIncorrect = round.incorrectGuesses.length;
-//   const percentCorrect = Math.round((1 - numIncorrect / numOfQuestions) * 100);
-//   return percentCorrect;
-// };
-
-// const endRound = round => {
-//   const percentCorrect = calculatePercentCorrect(round);
-//   return
-//   `** Round over! ** You answered ${percentCorrect}% of the questions correctly.`
-// }
-// test:
-// expect(console.log()).to.equal(
-//   `** Round over! ** You answered ${percentCorrect}% of the questions correctly.`
-//     );
-//   });
-// });
-
-  // give feedback, store id of wrong guesses
-  // use map to create an array, for creating a card
-  // const { prototypeData } = require('./src/data.js');
-//   takeTurn(guess, round): updates the turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
-// When a guess is made:
-// The turns count is updated, regardless of whether the guess is correct or incorrect
-// The next card becomes current card
-// Guess is evaluated. Incorrect guesses will be stored (via card’s the id) in an array of incorrectGuesses
-// Feedback is returned regarding whether the guess is incorrect or correct
-// calculatePercentCorrect(round): calculates and returns the percentage of correct guesses
-// endRound(round): prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
-// const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-// const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-// const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-// const deck = createDeck([card1, card2, card3]);
-// const round = createRound(deck)
-// round.deck;  // => [card1, card2, card3];
-// round.currentCard; // => { id: 1,
-                   //      question: 'What is Robbie\'s favorite animal',
-                   //      answers: ['sea otter', 'pug', 'capybara'],
-                   //      correctAnswer: 'sea otter'
-                   //    }
-// round.turns; // => 0
-// round.incorrectGuesses;  // => []
-// takeTurn('sea otter', round); // => 'correct!'
-// takeTurn('spleen', round);  // => 'incorrect!'
-// round.turns;  // => 2
-// round.incorrectGuesses;  // => [14]
-// round.currentCard;  // => { id: 12,
-            	      //      question: 'What is Travis\'s favorite stress reliever?',
-            	      //      answers: ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
-            	      //      correctAnswer: 'playing with bubble wrap'
-            	      //    }
-// calculatePercentCorrect(round);  // => 50
-
-  // if (guess === correctAnswer) {
-  //   console.log('Correct!')
-  //   return card.correctAnswer;
-  // } else {
-  //   console.log('Incorrect!')
-  //   return guess
-  // }
